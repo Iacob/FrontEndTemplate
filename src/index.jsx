6cc035fc71f999f91a28c90582fabf272f6c6771
Iacob <luo.yong.name@gmail.com>
2019-05-08 23:58:19 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,16 @@
 
 var a = <div></div>;
+
+class Component1 extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return <div>1</div>;
+  }
+}
+
+ReactDOM.render(<Component1 />, document.getElementById('div1'));
